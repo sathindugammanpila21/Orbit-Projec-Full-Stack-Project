f4849f1d1fa875386dbcbb5b1f457f6a86ea8bb8
Sathindu Gammanpila <10900355@students.plymouth.ac.uk>
2025-08-21 22:32:36 +0530

    Update Spinner.js

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,11 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Spinner() {
+/**
+ * Full-screen Spinner component
+ * Customizable size, border color, overlay color, and z-index.
+ */
+function Spinner({
+  size = 40,
+  borderColor = "border-gray-200",
+  overlayColor = "bg-black",
+  overlayOpacity = "opacity-70",
+  zIndex = "z-[9999]",
+}) {
   return (
-    <div className="fixed inset-0 bg-black opacity-70 flex items-center justify-center z-[9999]">
-      <div className="h-10 w-10 border-4 border-gray-200 border-solid border-t-transparent rounded-full animate-spin"></div>
+    <div
+      className={`fixed inset-0 ${overlayColor} ${overlayOpacity} flex items-center justify-center ${zIndex}`}
+    >
+      <div
+        className={`border-4 border-solid ${borderColor} border-t-transparent rounded-full animate-spin`}
+        style={{ height: size, width: size }}
+      ></div>
     </div>
   );
 }
 
+Spinner.propTypes = {
+  size: PropTypes.number,
+  borderColor: PropTypes.string,
+  overlayColor: PropTypes.string,
+  overlayOpacity: PropTypes.string,
+  zIndex: PropTypes.string,
+};
+
 export default Spinner;
+
