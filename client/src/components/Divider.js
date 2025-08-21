cbe3480fb2a5203cfe96d3f0bfa08d04496bd2e0
Sathindu Gammanpila <10900355@students.plymouth.ac.uk>
2025-08-21 12:19:59 +0530

    change a bit

--- client/src/components/Divider.js
+++ client/src/components/Divider.js
@@ -1,7 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Divider() {
-  return <div className="h-[1px] bg-gray-400 my-2"></div>;
+/**
+ * Divider component
+ * Allows customization of height, color, and margin.
+ */
+function Divider({ height = "1px", color = "bg-gray-400", margin = "my-2" }) {
+  return <div className={`${margin} ${color}`} style={{ height }} />;
 }
 
+Divider.propTypes = {
+  height: PropTypes.string,
+  color: PropTypes.string,
+  margin: PropTypes.string,
+};
+
 export default Divider;
+
