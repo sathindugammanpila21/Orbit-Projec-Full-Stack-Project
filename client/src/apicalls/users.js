a00f1ad21dbeb9bd7af8d0e8174e6c5443b20d61
Sathindu Gammanpila <10900355@students.plymouth.ac.uk>
2025-08-21 10:53:35 +0530

    update

--- client/src/apicalls/users.js
+++ client/src/apicalls/users.js
@@ -1,5 +1,17 @@
-const { apiRequest } = require(".");
+import { apiRequest } from ".";
 
-export const RegisterUser = async (payload) => apiRequest('post', '/api/users/register', payload);
-export const LoginUser = async (payload) => apiRequest('post', '/api/users/login', payload);
-export const GetLoggedInUser = async () => apiRequest('get', '/api/users/get-logged-in-user');
+const BASE_URL = "/api/users";
+
+/**
+ * User-related API functions
+ * All return: { success: boolean, data?: any, message?: string }
+ */
+
+export const registerUser = async (payload) => 
+  apiRequest("post", `${BASE_URL}/register`, payload);
+
+export const loginUser = async (payload) => 
+  apiRequest("post", `${BASE_URL}/login`, payload);
+
+export const getLoggedInUser = async () => 
+  apiRequest("get", `${BASE_URL}/get-logged-in-user`);
