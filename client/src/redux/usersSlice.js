b66e6f79c84d7b27b112d6ca51853a87ccc950fd
Sathindu Gammanpila <10900355@students.plymouth.ac.uk>
2025-08-22 08:51:32 +0530

    maked some changes

--- client/src/redux/usersSlice.js
+++ client/src/redux/usersSlice.js
@@ -1,25 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+/**
+ * Redux slice to manage user state, user list, and notifications.
+ */
+const initialState = {
+  user: null,          // Currently logged-in user object
+  allUsers: [],        // All users (for admin or project members)
+  notifications: [],   // Notifications for the current user
+};
+
 const usersSlice = createSlice({
   name: "users",
-  initialState: {
-    user: null,
-    allUsers: [],
-    notifications: [],
-  },
+  initialState,
   reducers: {
-    SetUser(state, action) {
+    /**
+     * Set the currently logged-in user
+     * @param {typeof initialState} state 
+     * @param {{ payload: object|null }} action 
+     */
+    setUser(state, action) {
       state.user = action.payload;
     },
-    SetAllUsers(state, action) {
+
+    /**
+     * Set the full list of users
+     * @param {typeof initialState} state 
+     * @param {{ payload: object[] }} action 
+     */
+    setAllUsers(state, action) {
       state.allUsers = action.payload;
     },
-    SetNotifications(state, action) {
+
+    /**
+     * Set notifications for the current user
+     * @param {typeof initialState} state 
+     * @param {{ payload: object[] }} action 
+     */
+    setNotifications(state, action) {
       state.notifications = action.payload;
-    }
+    },
+
+    /**
+     * Reset all user-related data (e.g. on logout)
+     * @param {typeof initialState} state 
+     */
+    resetUser(state) {
+      state.user = null;
+      state.allUsers = [];
+      state.notifications = [];
+    },
   },
 });
 
-export const { SetUser, SetAllUsers , SetNotifications} = usersSlice.actions;
-
+export const { setUser, setAllUsers, setNotifications, resetUser } = usersSlice.actions;
 export default usersSlice.reducer;
