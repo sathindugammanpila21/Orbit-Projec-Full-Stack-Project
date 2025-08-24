affc0bc79b6bae5e6362fb7c9c5a7435d1fff5e5
Sathindu Gammanpila <10900355@students.plymouth.ac.uk>
2025-08-24 23:34:30 +0530

    Update userModel.js

--- server/server/models/userModel.js
+++ server/server/models/userModel.js
@@ -4,24 +4,36 @@ const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      required: [true, "First name is required"],
+      trim: true,
     },
     lastName: {
       type: String,
-      required: true,
+      required: [true, "Last name is required"],
+      trim: true,
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       trim: true,
+      lowercase: true,
       unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+      index: true
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Password is required"],
+      select: false, // hides password by default when querying
     },
   },
   { timestamps: true }
 );
 
+// Virtual field for full name
+userSchema.virtual("fullName").get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
 module.exports = mongoose.model("users", userSchema);
+
